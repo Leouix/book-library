e07fa295f2178c38cbad562257942475b93632ab
leouix <nsht22sola@gmail.com>
2026-06-27 12:18:34 +0700

    db схема

---
+++ query.sql
@@ -0,0 +1,24 @@
+-- name: CreateBook :one
+INSERT INTO books (title, author, year)
+VALUES ($1, $2, $3)
+RETURNING id, title, author, year;
+
+-- name: GetBook :one
+SELECT id, title, author, year
+FROM books
+WHERE id = $1;
+
+-- name: ListBooks :many
+SELECT id, title, author, year
+FROM books
+ORDER BY id;
+
+-- name: UpdateBook :one
+UPDATE books
+SET title = $2, author = $3, year = $4
+WHERE id = $1
+RETURNING id, title, author, year;
+
+-- name: DeleteBook :exec
+DELETE FROM books
+WHERE id = $1;
